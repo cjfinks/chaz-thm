91d00fde83f630ff5f4ff20690134a655f48badc
Charles Garfinkle <cjfinks@gmail.com>
2017-11-21 14:13:08 -0800

    chaz

--- manuscripts/PNAS/second review/code/constants.py
+++ manuscripts/PNAS/second review/code/constants.py
@@ -2,6 +2,7 @@ from decomp_svd import subspace_angles
 import numpy as np
 import itertools
 import operator as op
+from scipy.misc import comb as choose
 
 def friedrichs(U,V):
     """
@@ -12,7 +13,7 @@ def friedrichs(U,V):
     Alternatively, it is the (s+1)th principal angle when dim(U \cap V) = s.
     """
     angles = subspace_angles(U,V)
-    nz_angles = angles > 1e-10 #> np.finfo(float).eps
+    nz_angles = angles > np.finfo(float).eps
     if np.any(nz_angles):
         min_nz_angle = np.sort(angles[nz_angles])[0]
         return min_nz_angle
@@ -39,10 +40,7 @@ def xi(A, G):
         prods.append(prod)
     return 1 - np.sqrt(1 - np.max(prods))
 
-#def powerset(s):
-#    return [[x for j, x in enumerate(s) if (i >> j) & 1] for i in range(2**len(s))]
-
-def C_2(A, H, r):
+def C2(A, H, r):
     """
     Ouputs the constant C2
 
@@ -56,13 +54,26 @@ def C_2(A, H, r):
     max_A = np.max( np.linalg.norm(A, axis=0) )
     return len(H) * max_A / min(xi_Gs)
 
-def C1_denom(A, X, H, k, num_rand_ksets=10 ** 2):
+def L_k(M, k, n_samples = None):
+    vals = []
+    if n_samples is None:
+        supports = itertools.combinations(range(M.shape[1]), k)
+    else:
+        supports = [np.random.choice(M.shape[1],k) for i in range(n_samples)]
+    for S in supports:
+        u, s, v = np.linalg.svd(M[:,S])
+        vals.append(s[-1])
+    return np.percentile(vals, 1 ) #min(np.array(vals)[np.array(vals) > 1e-14]) # #min(vals)
+
+def C1_denom(A, Xs, H, num_rand_ksets = None):
     """
     Ouputs the denominator in the def of constant C1
 
     ************
-    ASSUMES: first (k-1)(m choose k) are supported on H[0], etc
-             [so that N = |H|(k-1)(m choose k)]
+    ASSUMES: first (k-1)(m choose k) are supported on H[0],
+             second (k-1)(m choose k) are supported on H[1],
+             ...
+             so that N = |H|(k-1)(m choose k)
     ************
 
     A: n x m dictionary matrix (2d numpy array)
@@ -70,16 +81,14 @@ def C1_denom(A, X, H, k, num_rand_ksets=10 ** 2):
     H: hypergraph (list of lists in range(m))
     k: sparsity k of support sets
     """
+    k = len(H[0])
     m = A.shape[1]
-    minC1 = np.inf
-    num_supp = (k - 1) * choose(m, k)
-    for c, S in enumerate(H):
-        AX = np.dot(A, X[:, c * num_supp:(c + 1) * num_supp])
-        for trial in range(num_rand_ksets):
-            K = np.random.choice(num_supp, k)
-            u, s, v = np.linalg.svd(AX[:, K])
-            minC1 = min(minC1, s[-1])
-    return minC1
+    min_val = np.inf
+    for S, X in zip(H, Xs):
+        AX = np.dot(A[:,S], X)
+        lk = L_k(AX, k, n_samples = num_rand_ksets)
+        min_val = min(min_val, lk)
+    return min_val
 
 if __name__ == '__main__':
     #np.random.seed(0)
@@ -92,48 +101,41 @@ if __name__ == '__main__':
         ]
     r = 3
     """
-    H = [ # rows and cols of square array
-        [0,1,2,3],
-        [4,5,6,7],
-        [8,9,10,11],
-        [12,13,14,15],
-        [0,4,8,12],
-        [1,5,9,13],
-        [2,6,10,14],
-        [3,7,11,15]
-        ]
-    r = 2; k = 4; m = 16; n = 16
-    num_supp = (k - 1) * choose(m, k); N = len(H) * num_supp
-    num_trials = 3
+    """ rows and cols of square grid """
+    r = 2
+    k = 4;
+    m = k**2
+    n = m;
+    square_grid = np.reshape(range(m),(k,k))
+    H = [row.tolist() for row in square_grid] + [col.tolist() for col in square_grid.T]
+    N_per_support = int((k - 1) * choose(m, k))
+    N = len(H) * N_per_support
+    num_trials = 100
     Cs = np.zeros((num_trials, 2))  # 0:C1, 1:C2
-    pcntiles = np.zeros((num_trials, 2))  # 0:C1, 1:C2
+    #pcntiles = np.zeros((num_trials, 2))  # 0:C1, 1:C2
     for i in range(num_trials):
-        #A = np.eye(9,9)\
         print("Trial %d" % i)
         A = np.random.randn(n, m)
-        A = np.dot( A, np.diag(1./np.linalg.norm(A,axis=0)) ) # normalize
-
-        X = np.zeros((m, N))
-        for c, G in enumerate(H):
-            spots = np.random.permutation(m)[:k]
-            X[spots, c * num_supp:(c + 1) * num_supp] = np.random.randn(k, num_supp)
-        X = np.dot(X, np.diag(1. / np.linalg.norm(X, axis=0)))
-        Cs[i, 1] = C_2(A, H, r)
-        Cs[i, 0] = C1_denom(A, X, H, k)
-        #print('%1.3f' % c2)
+        A = np.dot( A, np.diag(1./np.linalg.norm(A, axis=0)) ) # normalize
+        Xs = [np.random.randn(k, N_per_support) for S in H]
+        Xs = [np.dot(X, np.diag(1. / np.linalg.norm(X, axis=0))) for X in Xs] # normalize
+        Cs[i, 1] = C2(A, H, r)
+        Cs[i, 0] = Cs[i,1] / C1_denom(A, Xs, H, num_rand_ksets = 10)
+        #Cs[i,0] = Cs[i,1] / L_k( np.dot(A[:,H[0]], Xs[0]), k, n_samples = 10 )
+       # print('%1.3f' % c2)
        # if (i > 1000) & (i % 100 == 0):
        #     print(i)
        #     pcntiles[i, 1] = np.percentile(Cs[:i, 1], 95)
 
     import matplotlib.pyplot as pp
     pp.ion()
-    plt.figure()
+    pp.figure()
     C2s = Cs[:, 1]
-    pp.hist(C2s[C2s<1000], bins=40)
+    pp.hist(C2s, bins=40)
     pp.show()
     pp.title('C2s')
 
-    plt.figure()
+    pp.figure()
     C1s = Cs[:, 0]
     pp.hist(C1s, bins=100)
     pp.show()
@@ -142,3 +144,6 @@ if __name__ == '__main__':
     # pp.figure();
     # pp.plot(pcntiles[:, 1])
     # pp.title('C2s 95 Percentiles over trials')  # looks funny for some reason
+
+### NOTE $$$
+# What is RIP of n by (k-1)* mCk matrix?
